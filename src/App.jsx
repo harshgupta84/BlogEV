7f0c2a02124b76b28d98bf7a6a757d22643d505e
Harsh G <harsh.g@eagleview.com>
2025-01-17 22:18:51 +0530

    change background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 import './App.css'
 import { Button } from './components/ui/button'
-
+import {DotPattern} from './components/ui/dot-pattern'
+import { cn } from './lib/utils'
 function App() {
   
   return (
@@ -10,8 +11,12 @@ function App() {
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-<Button>app</Button>
-
+          <Button className=' bg-white text-black'>app</Button>
+            <DotPattern
+                className={cn(
+                    "absolute inset-0 z-0 [mask-image:radial-gradient(50vw_circle_at_center,white,transparent)]"
+                )}
+            />
       </div> 
     
   )
