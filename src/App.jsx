efb98b73a4fbb74274abb82b174a797fcfa8c439
Harsh G <harsh.g@eagleview.com>
2025-01-17 23:56:51 +0530

    theme change

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Button } from './components/ui/button'
 import {DotPattern} from './components/ui/dot-pattern'
 import { cn } from './lib/utils'
+import ThemeSwitcher from './ThemeSwitcher'
 function App() {
   
   return (
@@ -11,12 +12,13 @@ function App() {
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-          <Button className=' bg-white text-black'>app</Button>
+          <Button className=' rounded-lg'>app</Button>
             <DotPattern
                 className={cn(
                     "absolute inset-0 z-0 [mask-image:radial-gradient(50vw_circle_at_center,white,transparent)]"
                 )}
             />
+             <ThemeSwitcher />
       </div> 
     
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { ThemeProvider } from './components/ui/ThemeContext'
 createRoot(document.getElementById('root')).render(
+  <ThemeProvider>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </ThemeProvider>
 )
