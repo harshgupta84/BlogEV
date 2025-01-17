b3eedd4dabe7e9e61959925ec14fa4a36d679c21
Harsh G <harsh.g@eagleview.com>
2025-01-17 14:53:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+  return (
+    
+      <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+<Button>app</Button>
+
+      </div> 
+    
+  )
+}
+
+export default App
+
+
+
+
