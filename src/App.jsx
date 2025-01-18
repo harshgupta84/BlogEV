6fa3cb65e49b0fda62b6494baeb9fd581b33759a
Harsh G <harsh.g@eagleview.com>
2025-01-18 11:24:49 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
-
-import './App.css'
-import { Button } from './components/ui/button'
-import {DotPattern} from './components/ui/dot-pattern'
-import { cn } from './lib/utils'
-import ThemeSwitcher from './ThemeSwitcher'
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { DotPattern } from "./components/ui/dot-pattern";
+import { cn } from "./lib/utils";
+import ThemeSwitcher from "./ThemeSwitcher";
 function App() {
-  
   return (
-    
-      <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-          <Button className=' rounded-lg'>app</Button>
-            <DotPattern
-                className={cn(
-                    "absolute inset-0 z-0 [mask-image:radial-gradient(50vw_circle_at_center,white,transparent)]"
-                )}
-            />
-             <ThemeSwitcher />
-      </div> 
-    
-  )
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button className=" rounded-lg">app</Button>
+      <DotPattern
+        className={cn(
+          "absolute inset-0 z-0 [mask-image:radial-gradient(50vw_circle_at_center,white,transparent)]"
+        )}
+      />
+      <ThemeSwitcher />
+    </div>
+  );
 }
 
-export default App
-
-
-
-
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ThemeProvider } from './components/ui/ThemeContext'
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ThemeProvider } from "./components/ui/ThemeContext";
+createRoot(document.getElementById("root")).render(
   <ThemeProvider>
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+      <App />
+    </StrictMode>
   </ThemeProvider>
-)
+);
