e5290b0c86177a1418c8b506ef033a445be5ffa9
harshgupta84 <21ucs084@lnmiit.ac.in>
2025-01-19 21:51:00 +0530

    chnages in path

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import LandingPage from "./pages/landingPage/LandingPage";
 import MyBlogs from "./pages/blogList/MyBlogs";
 import Blog from "./pages/blogView/Blog";
 import CreateBlog from "./pages/blogCreate/CreateBlog";
+import UpdateBlog from "./pages/blogUpdate/UpdateBlog";
+import Header from "./pages/components/Header";
 
 function App() {
   return (
     <div>
+    
       <BrowserRouter>
+      <Header/>
         <Routes>
             <Route path="/" Component={LandingPage}></Route>
             <Route path="/myblogs" Component={MyBlogs}></Route>
             <Route path="/blog/create" Component={CreateBlog}></Route>
             <Route path="/blog/view/:id" Component={Blog}></Route>
-            <Route path="/blog/update/:id" Component={CreateBlog}></Route>
+            <Route path="/blog/update/:id" Component={UpdateBlog}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/blogList/MyBlogs.jsx
+++ src/pages/blogList/MyBlogs.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "../components/Header";
+
 import BlogPosts from "./BlogPosts";
 
 function MyBlogs() {
   return (
-    <div className="">
-      <Header />
-     <div className="mt-24">
+    <div className="mt-24">
+    
+     <div className="">
         <h1 className=" text-5xl md:text-7xl max-w-3xl  text-center font-regular">
         
           <span className="bg-gradient-to-br tracking-tighter from-[#0098C5] to-[#8CCC4C] bg-clip-text text-transparent">
