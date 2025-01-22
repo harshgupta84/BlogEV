83ad52694361f2516b9487fedeb152deab91efff
Kambojharsh <kambojharsh2@gmail.com>
2025-01-22 17:03:35 +0530

    Extra imports and routes deleted

--- src/pages/blogCreate/CreateBlog.jsx
+++ src/pages/blogCreate/CreateBlog.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
-
 import Editor from './Editor'
-import { Toaster } from "@/components/ui/toaster";  
-import { ToastProvider } from '@radix-ui/react-toast';
+
 
 const CreateBlog = () => {
   return (
     <div>
         
         <div className="mt-24">
-          <ToastProvider> 
           <Editor />
-          <Toaster /> 
-          </ToastProvider>
         </div> 
        
     </div>
--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Hero from "./Hero";
-import Header from "../components/Header";
 
 function LandingPage() {
   return (
     <div>
-     
-      <Header />
       <Hero />
     </div>
   );
