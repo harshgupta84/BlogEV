be22c9d5c861547b64a35e754f38e678a202fc9c
Harsh G <harsh.g@eagleview.com>
2025-02-12 11:33:28 +0530

    chnages

--- src/pages/blogList/BlogPosts.jsx
+++ src/pages/blogList/BlogPosts.jsx
@@ -11,7 +11,7 @@ export default function BlogPosts({blogs}) {
       
       <div className=" mx-20">
         <div className="flex flex-col gap-6">
-          {blogs.toReversed().map((blog) => (
+          {blogs.map((blog) => (
             <BlogPostCard key={blog.id} blog={blog} />
           ))}
         </div>
